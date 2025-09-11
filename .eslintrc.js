998cc2fbe18cf275a6b9525634e6c0fedabd0890
Sagar <mrsagarshinde07@gmail.com>
2025-09-11 11:14:51 +0530

    sagar: added test file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': 'error'
+  },
+};
